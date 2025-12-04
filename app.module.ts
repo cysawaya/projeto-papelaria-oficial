41e145613893846ad69b4b3cc548bf201cfc7342
cysawaya <cynthiasawaya.ads@gmail.com>
2025-12-04 20:14:20 -0300

    Add files via upload

---
+++ app.module.ts
@@ -0,0 +1,33 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { ProductsModule } from './products/products.module';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+import { OrdersModule } from './orders/orders.module';
+import { PaymentsModule } from './payments/payments.module';
+import { Product } from './products/product.entity';
+import { User } from './users/user.entity';
+import { Order } from './orders/orders.entity';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
+      entities: [Product, User, Order],
+      synchronize: true,
+    }),
+    ProductsModule,
+    UsersModule,
+    AuthModule,
+    OrdersModule,
+    PaymentsModule,
+  ],
+})
+export class AppModule {}
